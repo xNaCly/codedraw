765b438ce09710ecdd56347e5d3b3ba083729c70
xnacly <47723417+xNaCly@users.noreply.github.com>
2024-06-15 14:34:09 +0200

    responsive interface

--- main.js
+++ main.js
@@ -21,8 +21,33 @@ class Draw {
     this.#board.height = 512;
     this.#board.width = 512;
   }
+
+  #execute(instructions) {}
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  let editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+    lineNumbers: true,
+    mode: "text/plain",
+    theme: "base16-light",
+  });
+  let args = new URLSearchParams(location.search).get("i");
+  let text = `// set the color to red
+COLOR #ff0000
+// draw a red box at 256 256
+BOX 256 256 1 1
+
+// set the color to green
+COLOR green
+// draw a line from 0x0 to 10x10
+LINE 0 0 10 10
+
+// write text at 128x128
+TEXT 128 128 Hello World`;
+
+  if (args?.length) {
+    text = atob(args);
+  }
+  editor.setOption("value", text);
   new Draw();
 });
