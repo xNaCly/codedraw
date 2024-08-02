e26238c64e16ae73f001081802980f97d5e355a9
xnacly <47723417+xNaCly@users.noreply.github.com>
2024-08-03 00:03:05 +0200

    add error handling and error display

--- main.js
+++ main.js
@@ -1,67 +1,79 @@
 /**
- * @typedef{{operation: string, arguments: (string|number)[]}} Instruction
- */
-
-/**
  * @param {string} input
  * @returns {Instruction[]}
  */
 function parser(input) {
+  /**
+   * @typedef{{operation: string, arguments: (string|number)[], line: number, column: number}} Instruction
+   */
   /**@type{Instruction[]}*/
   let r = [];
   let lines = input.split("\n");
-  for (let i = 0; i < lines.length; i++) {
-    let chars = lines[i].split("");
-    let lexemes = [];
-    for (let j = 0; j < chars.length; j++) {
-      let char = chars[j];
-      switch (char) {
-        // skip whitespace
-        case "\t":
-        case "":
-        case " ":
-          continue;
-        case "/": // skip comment
-          if (chars[j + 1] === "/") {
-            while (j < chars.length) {
-              j++;
-              char = chars[j];
-            }
+  let line = 0;
+  let column = 0;
+  try {
+    for (; line < lines.length; line++) {
+      let chars = lines[line].split("");
+      let lexemes = [];
+      for (column = 0; column < chars.length; column++) {
+        let char = chars[column];
+        switch (char) {
+          // skip whitespace
+          case "\t":
+          case "":
+          case " ":
             continue;
+          case "/": // skip comment
+            if (chars[column + 1] === "/") {
+              while (column < chars.length) {
+                column++;
+                char = chars[column];
+              }
+              continue;
+            }
+          case '"': {
+            column++; // skip "
+            char = chars[column];
+            let start = column;
+            while (column < chars.length && char !== '"') {
+              column++;
+              char = chars[column];
+            }
+            if (char !== '"') {
+              throw new Error("Unterminated string");
+            }
+            lexemes.push(lines[line].substring(start, column++));
+            break;
           }
-        case '"': {
-          j++; // skip "
-          char = chars[j];
-          let start = j;
-          while (j < chars.length && char !== '"') {
-            j++;
-            char = chars[j];
-          }
-          if (char !== '"') {
-            throw new Error(`Unterminated string in line ${i} and column ${j}`);
-          }
-          lexemes.push(lines[i].substring(start, j++));
-          break;
-        }
-        default: {
-          let start = j;
-          while (j < chars.length && char !== " ") {
-            j++;
-            char = chars[j];
-          }
-          let l = lines[i].substring(start, j);
-          let firstChar = l.charAt(0);
-          if (firstChar >= "0" && firstChar <= "9") {
-            lexemes.push(parseInt(l));
-          } else {
-            lexemes.push(l);
+          default: {
+            let start = column;
+            while (column < chars.length && char !== " ") {
+              column++;
+              char = chars[column];
+            }
+            let l = lines[line].substring(start, column);
+            let firstChar = l.charAt(0);
+            if (firstChar >= "0" && firstChar <= "9") {
+              lexemes.push(parseInt(l));
+            } else {
+              lexemes.push(l);
+            }
           }
         }
       }
+      if (lexemes.length) {
+        r.push({
+          operation: lexemes[0],
+          arguments: lexemes.slice(1),
+          line,
+          column,
+        });
+      }
     }
-    if (lexemes.length) {
-      r.push({ operation: lexemes[0], arguments: lexemes.slice(1) });
-    }
+  } catch (e) {
+    throw new Error(
+      `${e.message} in line ${line + 1} and column ${column + 1}`
+    );
   }
   return r;
 }
@@ -93,57 +105,65 @@ class Draw {
     this.#ctx.clearRect(0, 0, this.board.width, this.board.height);
     for (let i = 0; i < instructions.length; i++) {
       let instruction = instructions[i];
-      switch (instruction.operation) {
-        case "COLOR":
-          if (instruction.arguments.length !== 1) {
-            throw new Error("COLOR requires exactly one argument");
+      try {
+        switch (instruction.operation) {
+          case "COLOR":
+            if (instruction.arguments.length !== 1) {
+              throw new Error("COLOR requires exactly one argument");
+            }
+            this.#ctx.fillStyle = instruction.arguments[0];
+            this.#ctx.strokeStyle = instruction.arguments[0];
+            break;
+          case "BOX": {
+            if (instruction.arguments.length !== 4) {
+              throw new Error("BOX requires exactly four arguments (x,y,w,h)");
+            }
+            let [x, y, w, h] = instruction.arguments;
+            this.#ctx.fillRect(x, y, w, h);
+            break;
           }
-          this.#ctx.fillStyle = instruction.arguments[0];
-          this.#ctx.strokeStyle = instruction.arguments[0];
-          break;
-        case "BOX": {
-          if (instruction.arguments.length !== 4) {
-            throw new Error("BOX requires exactly four arguments (x,y,w,h)");
+          case "WIDTH": {
+            if (instruction.arguments.length !== 1) {
+              throw new Error("WIDTH requires exactly one argument (width)");
+            }
+            this.#ctx.lineWidth = instruction.arguments[0];
+            break;
           }
-          let [x, y, w, h] = instruction.arguments;
-          this.#ctx.fillRect(x, y, w, h);
-          break;
-        }
-        case "WIDTH": {
-          if (instruction.arguments.length !== 1) {
-            throw new Error("WIDTH requires exactly one argument (width)");
+          case "LINE": {
+            if (instruction.arguments.length !== 4) {
+              throw new Error(
+                "LINE requires exactly four arguments (sx,sy,ex,ey)"
+              );
+            }
+            let [x, y, x1, y1] = instruction.arguments;
+            this.#ctx.beginPath();
+            this.#ctx.moveTo(x, y);
+            this.#ctx.lineTo(x1, y1);
+            this.#ctx.stroke();
+            break;
           }
-          this.#ctx.lineWidth = instruction.arguments[0];
-          break;
-        }
-        case "LINE": {
-          if (instruction.arguments.length !== 4) {
+          case "TEXT": {
+            if (instruction.arguments.length < 3) {
+              throw new Error(
+                "TEXT requires at least three arguments (x,y,text, font)"
+              );
+            }
+            let [x, y, text, font] = instruction.arguments;
+            if (font) this.#ctx.font = font;
+            this.#ctx.fillText(text, x, y);
+            break;
+          }
+          default:
             throw new Error(
-              "LINE requires exactly four arguments (sx,sy,ex,ey)"
+              `Operation "${instruction.operation}" not defined (args:${instruction.arguments})`
             );
-          }
-          let [x, y, x1, y1] = instruction.arguments;
-          this.#ctx.beginPath();
-          this.#ctx.moveTo(x, y);
-          this.#ctx.lineTo(x1, y1);
-          this.#ctx.stroke();
-          break;
         }
-        case "TEXT": {
-          if (instruction.arguments.length < 3) {
-            throw new Error(
-              "TEXT requires at least three arguments (x,y,text, font)"
-            );
-          }
-          let [x, y, text, font] = instruction.arguments;
-          if (font) this.#ctx.font = font;
-          this.#ctx.fillText(text, x, y);
-          break;
-        }
-        default:
-          throw new Error(
-            `Operation "${instruction.operation}" not defined (args:${instruction.arguments})`
-          );
+        updateToast("");
+      } catch (e) {
+        let { line, column } = instruction;
+        throw new Error(
+          `${e.message} in line ${line + 1} and column ${column + 1}`
+        );
       }
     }
   }
@@ -151,6 +171,17 @@ class Draw {
 
 let editor = null;
 let draw = null;
+/**@type{HTMLSpanElement|null}*/
+let errorToast = null;
+
+function updateToast(text) {
+  if (text) {
+    errorToast.innerText = text;
+    errorToast.classList.remove("hidden");
+  } else {
+    errorToast.classList.add("hidden");
+  }
+}
 
 function shareURL() {
   if (!navigator.clipboard) {
@@ -166,12 +197,19 @@ function exportImg() {
   window.open(url, "_blank");
 }
 
+window.onerror = (msg) => {
+  updateToast(msg);
+  return false;
+};
+
 document.addEventListener("DOMContentLoaded", () => {
+  errorToast = document.getElementById("error");
   editor = CodeMirror.fromTextArea(document.getElementById("code"), {
     lineNumbers: true,
     mode: "text/plain",
     theme: "base16-light",
   });
+
   let args = new URLSearchParams(location.search).get("i");
   let text = `// set the color to red
 COLOR #ff0000
@@ -194,6 +232,7 @@ TEXT 128 128 "Hello World"`;
       text = t;
     }
   }
+
   draw = new Draw();
   editor.on("update", function () {
     let val = editor.getValue();
